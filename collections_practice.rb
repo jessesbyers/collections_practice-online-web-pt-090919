43992d0412c7dff40ed0c3e2b32252a9613fad52
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-13 17:31:36 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,5 +13,7 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  array[1].sort(array[2])
+  array.sort do |array[1],array[2]|
+    array[1],array[2] = array[2],array[1]
+  end
 end
